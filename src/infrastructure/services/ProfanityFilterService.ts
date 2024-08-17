79743792392d6381ddb8fb0afb20e861bd12d36d
Deivison Oliveira Costa <112001225+Deivison-Costa@users.noreply.github.com>
2024-08-17 20:55:38 -0300

    [refactor]: corrected tabulation

--- src/infrastructure/services/ProfanityFilterService.ts
+++ src/infrastructure/services/ProfanityFilterService.ts
@@ -1,20 +1,20 @@
 import {
-    RegExpMatcher,
-    TextCensor,
-    englishDataset,
-    englishRecommendedTransformers,
-  } from "obscenity"
+  RegExpMatcher,
+  TextCensor,
+  englishDataset,
+  englishRecommendedTransformers,
+} from "obscenity"
   
-  export class ProfanityFilterService {
-    private matcher = new RegExpMatcher({
-      ...englishDataset.build(),
-      ...englishRecommendedTransformers,
-    })
-    private censor = new TextCensor()
+export class ProfanityFilterService {
+  private matcher = new RegExpMatcher({
+    ...englishDataset.build(),
+    ...englishRecommendedTransformers,
+  })
+  private censor = new TextCensor()
   
-    clean(text: string): string {
-      const matches = this.matcher.getAllMatches(text)
-      return this.censor.applyTo(text, matches)
-    }
+  clean(text: string): string {
+    const matches = this.matcher.getAllMatches(text)
+    return this.censor.applyTo(text, matches)
   }
+}
   
